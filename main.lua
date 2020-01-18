49525dc3780646615741b250e9c33599691d12d5
Daniel Křemen <daniel.kremen@outlook.cz>
2020-01-18 20:42:49 +0100

    + not finished yet

--- main.lua
+++ main.lua
@@ -12,3 +12,36 @@ SlashCmdList["PRICE"] = function(lnk)
 		print("Wrong item link specified")
 	end
 end
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("LOOT_OPENED")
+frame:SetScript("OnEvent", function(self, event, arg1)
+	print("Minimal DKP values: ")
+	
+	for i=1,GetNumLootItems() do
+
+		print(i)
+		--if LootSlotHasItem(i) then
+
+			local lootIcon, lootName, lootQuantity, currencyID, lootQuality, locked, isQuestItem, questID, isActive = GetLootSlotInfo(i)			
+			local hasItem = LootSlotHasItem(i)
+			
+			if hasItem then
+				local itemLink = GetLootSlotLink(i)
+				
+				if itemLink then
+					local linkstext = ""
+					linkstext=linkstext .. itemLink
+					SendChatMessage(linkstext, "SAY", nil, nil)
+					local itemIdstr = string.match(linkstext, "item[%-?%d:]+")
+					SendChatMessage(itemIdstr, "SAY", nil, nil)
+
+					local _, id = strsplit(":", itemIdstr)
+					SendChatMessage(id, "SAY", nil,nil)
+				end
+			end
+			
+		--end if
+	end
+
+end)
